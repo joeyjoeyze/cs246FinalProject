aa8ada8ab9e5493cf174f833b133887c325b5080
arladerus <kevinchen@live.ca>
2013-07-25 00:13:42 -0400

    Update block.h

--- block.h
+++ block.h
@@ -8,13 +8,14 @@ class Block {
     char type;					//which type of block is it
 	Cell * parts[tetris];			//the four parts of a tetric block
 	Board * board;				//need this to invoke the moveCell command in board
+	int alive;				//count of number of alive cells; starts at 4
 								//need some transformation parameters
  public:
 	Block();					//non-virtual ctor & dtor
 	~Block();
-    void rotate(bool)=0;		//parameter determines clockwise or counterclockwise rotation
-	void shift(const int&)=0;	//paramenter indicates which direction of motion
-	bool canMove()=0;			//determines if the block is able to move at its current location
+    	void rotate(bool);		//parameter determines clockwise or counterclockwise rotation
+	void shift(const int&);		//paramenter indicates which direction of motion
+	bool canMove();			//determines if the block is able to move at its current location
 };
 
 #endif
