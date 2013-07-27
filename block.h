048110c98161f9a707d34002e09e31e655aef110
arladerus <kevinchen@live.ca>
2013-07-27 18:02:24 -0400

    Added colour, fixed constructor

--- block.h
+++ block.h
@@ -8,10 +8,11 @@ class Block {
     char type;					//which type of block is it
 	Cell * parts[tetris];			//the four parts of a tetric block
 	Board * board;				//need this to invoke the moveCell command in board
+	int colour;				//colour of the blocks
 	int alive;				//count of number of alive cells; starts at 4
 								//need some transformation parameters
  public:
-	Block();					//non-virtual ctor & dtor
+	Block::Block(Board* board=0, char type=0, int colour=0, Cell *parts[]=0); //non-virtual ctor & dtor
 	~Block();
     	void rotate(bool);		//parameter determines clockwise or counterclockwise rotation
 	void shift(const int&);		//paramenter indicates which direction of motion
