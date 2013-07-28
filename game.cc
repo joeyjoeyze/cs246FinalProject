20074d1167f90bfd246d51bf683fd44268a5607d
joeyjoeyze <joeyjoeyze@hotmail.com>
2013-07-27 22:47:46 -0400

    updated game class

--- game.cc
+++ game.cc
@@ -2,20 +2,26 @@
 using namespace std;
 
 Game::Game(const int& level=0, bool GUI=true)
-:score(0),level(level),initialLevel(level),highScore(0),inFile(NULL){
+:score(0),level(level),highScore(0),initialLevel(level){
 	board = new Board;
-
+	randBlock = new RandomBlock(level);
 }
 
 Game::Game(const String& fileName, const int& level=0, bool GUI=true)
-:score(0),level(level),initialLevel(level),highScore(0){
-	inFile->open(fileName.c_str());
+:score(0),level(level),highScore(0),initialLevel(level){
 	board = new Board;
-	
+	randBlock = new RandomBlock(fileName,level);
 }
 
 Game::~Game(){
+	for(vector<Block *>::iterator vi=blocks.begin();vi!=blocks.end();++vi){
+	//using iterators to delete all the block pointers in Blocks
+		delete *vi
+	}
+	blocks.clear();
+	delete randBlock;
 	delete board;
+	
 }
 
 void updateScore(const int& newScore){	//called when a row is cancelled, could implement without parameter
@@ -24,27 +30,58 @@ void updateScore(const int& newScore){	//called when a row is cancelled, could i
 }
 
 bool Game:command(const string& cmd){	//finds and calls the command from main in block, return value determines to spawn a new block or not
-	
+	Block * currentBlock = blocks[(blocks.size() - 2)];
+	if(cmd == "left"){
+		currentBlock.shift(board, 1);
+	}else if(cmd == "right"){
+		currentBlock.shift(board, 2);
+	}else if(cmd == "down"){
+		currentBlock.shift(board, 0);
+	}else if(cmd == "clockwise"){
+		currentBlock.rotate(board, 1);
+	}else if(cmd == "counterclockwise"){
+		currentBlock.rotate(board, 0);
+	}else if(cmd == "drop"){
+		for(int i=0;i<15;i++){
+			currentBlock.shift(board, 0);
+		}
+		blocks.push_back(newBlock());
+	}else if(cmd == "levelup"){
+		randBlock->levelUp();
+	}else if(cmd == "leveldown"){
+		randBlock->levelDown();
+	}else if(cmd == "restart"){
+		delete board;
+		board = new Board;
+		score = 0;
+		level = initLevel;
+		highScore = 0;
+		randBlock->setLevel(level);
+		for(vector<Block *>::iterator vi=blocks.begin();vi!=blocks.end();++vi){
+		//using iterators to delete all the block pointers in Blocks
+			delete *vi
+		}
+		blocks.clear();
+		blocks.push_back(newBlock());
+		blocks.push_back(newBlock());
+	}else{
+		cerr << "BAD COMMAND" << endl;
+	}
 }
 
-void Game::restart(){
-	score = 0;
-	level = 0;
-	highScore = 0;
-	delete board;
-	board = new Board;
-	
+void newBlock(){
+	Block * temp = randBlock->getBlock();
+	temp.setParts();
 }
-
-
 
 ostream& operator<<(ostream& out, const Game& g){
-		out << "Level:\t\t" << g.level << endl;
-		out << "Score:\t\t" << g.score << endl;
-		out << "Hi Score:\t" << g.highScore << endl;
-		out << "----------" << endl;
-		out << g.board << endl;
-		out << "----------" << endl;
-		out << g.nextBlock << endl;
-		return out;
-	}
+	out << "Level:" << setw(spacing + 3) << g.level << endl;
+	out << "Score:" << setw(sapcing + 3) << g.score << endl;
+	out << "Hi Score:" << setw(spacing) << g.highScore << endl;
+	out << "----------" << endl;
+	out << g.board << endl;
+	out << "----------" << endl;
+	out << "Next:" << endl;
+	out << g.blocks[(blocks.size() - 1)] << endl;
+	return out;
+}
--- game.h
+++ game.h
@@ -5,19 +5,23 @@
 #include "board.h"
 #include "randomblock.h"
 #include<string>
-
+#include<iostream>
+#include<iomanip>
+static const int spacing = 4;			//padding spacing to right align text
 class Game{
-	int score;				//score of the current game, implement this in player for multiplayer
-	int level;				//the current level of the game;
-	int highScore;			//current high score
-	int initLevel;			//initial level stated on the command line if non-zero
-	std::vector<Block *> blocks;	//the vector of blocks
-	Board * board;			//the board object that holds the game matrix
+	int score;							//score of the current game, implement this in player for multiplayer
+	int level;							//the current level of the game;
+	int highScore;						//current high score
+	int initLevel;						//initial level stated on the command line if non-zero
+	std::vector<Block *> blocks;		//the vector of blocks
+	RandomBlock * randBlock;
+	Board * board;						//the board object that holds the game matrix
 	public:
-	Game(bool GUI);
+	Game(const int& level=0, bool GUI=true)
+	Game(const String& fileName, const int& level=0, bool GUI=true)
 	~Game();
-	Block nextBlock();				//randomly generates a block
-	void updateScore(const int&);	//called when a row is cancelled, could implement without parameter
+	Block nextBlock();					//randomly generates a block
+	void updateScore(const int&);		//called when a row is cancelled, could implement without parameter
 	bool command(const std::String&);	//finds and calls the command from main in block, return value determines to spawn a new block or not
 	friend std::ostream& operator<<(std::ostream&, const Game&);
 };
