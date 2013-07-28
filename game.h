2ce15ea4dfab70a11947b85621069fcf18f655cd
arladerus <kevinchen@live.ca>
2013-07-28 02:40:58 -0400

    bug fix

--- game.h
+++ game.h
@@ -13,14 +13,14 @@ class Game{
 	int level;							//the current level of the game;
 	int highScore;						//current high score
 	int initLevel;						//initial level stated on the command line if non-zero
+	bool GUI;
 	std::vector<Block *> blocks;		//the vector of blocks
 	RandomBlock * randBlock;
 	Board * board;						//the board object that holds the game matrix
-	public:
+  public:
 	Game(const int& level=0, bool GUI=true);
 	Game(const std::string& fileName, const int& level=0, bool GUI=true);
 	~Game();
-	Block nextBlock();					//randomly generates a block
 	void updateScore(const int&);		//called when a row is cancelled, could implement without parameter
 	bool command(const std::string&);	//finds and calls the command from main in block, return value determines to spawn a new block or not
 	friend std::ostream& operator<<(std::ostream&, const Game&);
