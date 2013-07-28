2a34981cbc8b78c36a095cfb54f6b5e44c9e9825
arladerus <kevinchen@live.ca>
2013-07-27 21:39:44 -0400

    Removed the pointer Board* board, and added as parameters to rotate and shift.

--- block.h
+++ block.h
@@ -7,17 +7,15 @@ static const int tetris = 4;
 class Block {
     char type;					//which type of block is it
 	Cell * parts[tetris];			//the four parts of a tetric block
-	Board * board;				//need this to invoke the moveCell command in board
 	int colour;				//colour of the blocks
 	int alive;				//count of number of alive cells; starts at 4
 								//need some transformation parameters
  public:
 	Block::Block(Board* board=0, char type=0, int colour=0, Cell *parts[]=0); //non-virtual ctor & dtor
 	~Block();
-    	void rotate(bool);		//parameter determines clockwise or counterclockwise rotation
-	void shift(const int&);		//paramenter indicates which direction of motion
-	bool canMove();			//determines if the block is able to move at its current location
-	void notify();			//notifies the board
+    	void rotate(Board* board, bool);	//parameter determines clockwise or counterclockwise rotation
+	void shift(Board* board, const int&);	//paramenter indicates which direction of motion
+	void notify();				//notifies the board
 };
 
 #endif
